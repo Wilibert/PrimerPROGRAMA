c961f14f436bf830b346b6582044d73688ca5f73
Laboratorio 109 Pc-17 <lab-109-inf-17>
2024-01-17 21:37:48 -0400

    primer compromiso

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        String nombre;
+        double monto;
+        double resultado;
+
+        nombre= "wilibert";
+          monto=475.50;
+        resultado= monto * 1.15;
+        System.out.println("hola " + nombre + "el valor total es" + resultado);
+
+    }
+
+}
